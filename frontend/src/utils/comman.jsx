9dea7c0b627e3d7f0d628d08fd84ad28771796b2
amanrawat08 <116343624+amanrawat08@users.noreply.github.com>
2026-02-25 13:13:14 +0530

    Remove cloud API keys from comman.jsx
    Removed sensitive cloud API keys from comman.jsx

--- frontend/src/utils/comman.jsx
+++ frontend/src/utils/comman.jsx
@@ -5,9 +5,7 @@ export const GET_POSTEDJOB_URL = "http://localhost:3000/api/jobs/getPostedJobs";
 export const DELETE_POSTEDJOB_URL = "http://localhost:3000/api/jobs/delete";
 export const UPDATE_POSTEDJOB_URL = "http://localhost:3000/api/jobs";
 export const UPDATE_USER_URL = "http://localhost:3000/api/users/updateUser";
-export const CLOUD_API_KEY = "153913823643653"
-export const CLOUD_API_NAME = "dyssfx9uw"
-export const CLOUD_API_SECRET_KEY = "Kd9-hgtxnMpX1ZQt6aew0VT10pk"
+ 
 
 //application routes
 export const APPLY_JOB_URL = "http://localhost:3000/api/application/apply";
