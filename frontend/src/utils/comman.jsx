9b59bc099f4941040d91cb24b59d6d76d45f5654
amanrawat08 <116343624+amanrawat08@users.noreply.github.com>
2026-03-21 22:54:11 +0530

    Update API URLs to production environment

--- frontend/src/utils/comman.jsx
+++ frontend/src/utils/comman.jsx
@@ -1,17 +1,17 @@
-export const REGISTER_USER_URL = `http://localhost:3000/api/users/register`;
-export const LOGIN_USER_URL = "http://localhost:3000/api/users/login";
-export const JOB_POST_URL = "http://localhost:3000/api/jobs";
-export const GET_POSTEDJOB_URL = "http://localhost:3000/api/jobs/getPostedJobs";
-export const DELETE_POSTEDJOB_URL = "http://localhost:3000/api/jobs/delete";
-export const UPDATE_POSTEDJOB_URL = "http://localhost:3000/api/jobs";
-export const UPDATE_USER_URL = "http://localhost:3000/api/users/updateUser";
+export const REGISTER_USER_URL = `https://job-portal-6cpj.onrender.com/api/users/register`;
+export const LOGIN_USER_URL = "https://job-portal-6cpj.onrender.com/api/users/login";
+export const JOB_POST_URL = "https://job-portal-6cpj.onrender.com/api/jobs";
+export const GET_POSTEDJOB_URL = "https://job-portal-6cpj.onrender.com/api/jobs/getPostedJobs";
+export const DELETE_POSTEDJOB_URL = "https://job-portal-6cpj.onrender.com/api/jobs/delete";
+export const UPDATE_POSTEDJOB_URL = "https://job-portal-6cpj.onrender.com/api/jobs";
+export const UPDATE_USER_URL = "https://job-portal-6cpj.onrender.com/api/users/updateUser";
  export const CLOUD_API_KEY = "153913823643653"
 export const CLOUD_API_NAME = "dyssfx9uw"
 export const CLOUD_API_SECRET_KEY = "Kd9-hgtxnMpX1ZQt6aew0VT10pk"
 
 //application routes
-export const APPLY_JOB_URL = "http://localhost:3000/api/application/apply";
-export const APPLICANT_JOB_URL = "http://localhost:3000/api/application/getApplications";
-export const UPDATE_APPLICANT_STATUS_URL = "http://localhost:3000/api/application/updateApplicantDetails";
-export const GET_MY_APPLICATION_STATUS_URL = "http://localhost:3000/api/application/my";
+export const APPLY_JOB_URL = "https://job-portal-6cpj.onrender.com/api/application/apply";
+export const APPLICANT_JOB_URL = "https://job-portal-6cpj.onrender.com/api/application/getApplications";
+export const UPDATE_APPLICANT_STATUS_URL = "https://job-portal-6cpj.onrender.com/api/application/updateApplicantDetails";
+export const GET_MY_APPLICATION_STATUS_URL = "https://job-portal-6cpj.onrender.com/api/application/my";
 
