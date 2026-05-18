be650bdd82d60670b5e4710670cfd9c14fad465a
amanrawat08 <ar7541147@gmail.com>
2026-05-18 15:56:39 +0530

    Deploy Changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
  
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-0zcn.onrender.com/",
   credentials: true
 }));
 app.use(express.json());
